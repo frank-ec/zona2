c412cad8cc6dff067b8f559354dc0e8b5673c296
frank-ec <franklynlara@hotmail.com>
2022-03-04 21:20:24 -0500

    v36 fontawesome.js

--- Views/Template/header_admin.php
+++ Views/Template/header_admin.php
@@ -13,8 +13,6 @@
     <!-- Main CSS F@ carga estilos de Assets -->
     <link rel="stylesheet" type="text/css" href="<?= media();?>/css/main.css">
     <link rel="stylesheet" type="text/css" href="<?= media();?>/css/style.css">
-    <!-- Font-icon css-->
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body class="app sidebar-mini">
     <!-- Navbar-->

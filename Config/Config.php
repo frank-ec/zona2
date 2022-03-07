ff6b4ad9b6db959c8fb5e078ed55a11ce56e6256
Frank-ec <franklynlara@hotmail.com>
2022-03-07 15:27:19 -0500

    ok

--- Config/Config.php
+++ Config/Config.php
@@ -10,7 +10,7 @@
 	const DB_HOST = "localhost";
 	const DB_NAME = "db_tiendavirtual";
 	const DB_USER = "root";
-	const DB_PASSWORD = "";
+	const DB_PASSWORD = "3201442";
 	const DB_CHARSET = "charset=utf8";
 
 	//Deliminadores decimal y millar Ej. 24,1989.00
